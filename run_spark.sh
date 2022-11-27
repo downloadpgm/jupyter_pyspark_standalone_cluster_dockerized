224fb9dc5929e74459fbf0aa01f14cb3e9a695c1
mkenjis <mkenjis@gmail.com>
2022-11-27 18:22:16 -0300

    Refactory using prebuilt java image

--- run_spark.sh
+++ run_spark.sh
@@ -18,20 +18,16 @@ ssh-keyscan 0.0.0.0 >>~/.ssh/known_hosts
 
 if [ -n "${SPARK_HOST_SLAVES}" ]; then
 
-   sleep 30
-      
+   sleep 20
+
    >${SPARK_HOME}/conf/slaves
-   
+
    for SPARK_HOST in `echo ${SPARK_HOST_SLAVES} | tr ',' ' '`; do
-      ssh-keyscan ${SPARK_HOST} >~/.ssh/known_hosts
-	  ssh root@${SPARK_HOST} "cat /etc/hostname" >>${SPARK_HOME}/conf/slaves
+      ssh-keyscan ${SPARK_HOST} >>~/.ssh/known_hosts
+          ssh root@${SPARK_HOST} "cat /etc/hostname" >>${SPARK_HOME}/conf/slaves
    done
-   
+
    # start Spark master and slaves nodes
    $SPARK_HOME/sbin/start-master.sh
    $SPARK_HOME/sbin/start-slaves.sh
 fi
-
-while [ 1 -eq 1 ]; do
-   sleep 10
-done
